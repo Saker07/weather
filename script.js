295703dea386acaea84f2274246437de0c7febd3
saker <gjoni.klaudio@gmail.com>
2022-08-31 22:05:26 +0100

    Make basic design

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 let k = "f4e0c249a3e57cabeea79c16f689c682";
+let unit = "metric";
 console.log("ciao");
 async function getData(){
     try {
-        let a = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=f4e0c249a3e57cabeea79c16f689c682`, {mode: "cors"})
+        let locationRequest = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=CrEmA&limit=5&appid=f4e0c249a3e57cabeea79c16f689c682`, {mode: "cors"})
 
-        a = await a.json();
-        console.log(a);
-        let lat = a[0].lat;
-        let lon = a[0].lon;
+        locationRequest = await locationRequest.json();
+        let lat = locationRequest[0].lat;
+        let lon = locationRequest[0].lon;
         console.log(lat);
-        console.log(lon)
+        console.log(lon);
 
-        lat = 51;
-        lon = 0;
-
-        let b = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${k}`, {mode: "cors"})
-        /* b = await b.json();
-        console.log(b); */
-            /* .then((response) => response.json())
-            .then((data) => console.log(data)); */
-            .then((response) => response.json())
-            .then((data) => console.log(data));
+        /* lat = 51;
+        lon = 0; */
+        let dataRequest = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${k}&units=${unit}`, {mode: "cors"});
+        dataRequest = await dataRequest.json();
+        console.log(dataRequest)
+        return dataRequest;
     } catch (error) {console.log(error)}
 }
+async function displayWeatherData(response){
+    document.querySelector(".mainWeather").textContent = response.weather[0].main.toUpperCase();
+    document.querySelector(".temp").textContent = response.main.temp;
+    document.querySelector(".feltTemp").textContent = response.main.feels_like;
+    document.querySelector(".humidity").textContent = response.main.humidity;
+    document.querySelector(".wind").textContent = response.wind.speed;
+    document.querySelector(".location").textContent = response.name.toUpperCase();
+}
 
-getData();
+getData().then(displayWeatherData);
